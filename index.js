fa0aebe17afdfd21c117923dc84bdce9adaab65b
J-aze <jerome.aze@umontpellier.fr>
2023-02-21 12:54:54 +0100

    basic server

--- index.js
+++ index.js
@@ -2,27 +2,19 @@
 
 const express = require("express");
 const app = express();
-const http = require("http");
 const server = http.createServer(app);
-const path = require("path");
 const { Server } = require("socket.io");
 const io = new Server(server);
-const morgan = require("morgan");
 
 const port = 3000;
 
-app.use("/css", express.static(path.join(__dirname, "public/css")));
-app.use("/js", express.static(path.join(__dirname, "public/js")));
-app.use("/img", express.static(path.join(__dirname, "public/img")));
-
-app.use(morgan("dev"));
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.send("hello");
 });
 
 io.on("connect", (socket) => {
   // console.log("a user connected");
+  io.emit("hello", "welcome");
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
     io.emit("chat message", msg);
